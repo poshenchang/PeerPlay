265ae6707ccce9ce23053581264015f26ae03d37
shericWu <shihyu1127@gmail.com>
2026-05-27 10:18:02 +0800

    feat: implement sketch of commit, reveal, verify for play_card in dealing module. todo: include keys

--- dealing/dealing.py
+++ dealing/dealing.py
@@ -1,17 +1,17 @@
 import random
-from typing import List
+from typing import List, Dict, Tuple
 from ecdsa.ellipticcurve import Point
 from ..consensus.consensus import ConsensusModule
 from ..network.network import (
     RawMessage,
     MSG_TYPE_SHUFFLE, MSG_TYPE_TAG,
-    MSG_TYPE_DETAG, MSG_TYPE_FINALDEAL
+    MSG_TYPE_DETAG, MSG_TYPE_FINALDEAL,
+    MSG_TYPE_COMMIT, MSG_TYPE_REVEAL
 )
 from ..utils.crypto import (
     gen_scalar_keypair,
     ec_multiply, ec_mod_inverse,
-    map_to_curve, map_from_curve,
-    hash_concat
+    map_to_curve, map_from_curve
 )
 
 def encrypt_point(point: Point, key: int) -> Point:
@@ -19,6 +19,16 @@ def encrypt_point(point: Point, key: int) -> Point:
 
 def decrypt_point(point: Point, key: int) -> Point:
     return ec_multiply(point, ec_mod_inverse(key))
+
+class DealingError(Exception):
+    """
+    Raised when dealing cannot complete
+    """
+
+class PlayCardError(Exception):
+    """
+    Raised when play card cannot complete
+    """
 
 class DealingModule:
     def __init__(self, consensus: ConsensusModule) -> None:
@@ -39,18 +49,10 @@ class DealingModule:
         self._decrypt_hand(hand_size)
         return self.hand
 
-    # TODO: return error if invalid?
     def play_card(self, card: int) -> None:
-        # TODO: commit: hash(action | nonce | tkey)
-        nonce = self.consensus.commitment.commit(card)
-        # TODO: listen
-        # TODO: reveal: card, nonce, tkey
-        self.consensus.commitment.reveal(card, nonce)
-        # TODO: verify: card, nonce, tkey[i]
-        #   (1) hash(card | nonce | tkey[i]) == commit
-        #   (2) encrypt_point(map_to_curve(card), tkey[i]) == _points
-        expected = hash_concat(card, nonce)
-        self.consensus.commitment.verify(card, nonce, expected)
+        nonce, commit_msgs = self._commit_played_card(card)
+        reveal_msgs = self._reveal_played_card(card, nonce)
+        self._verify_played_cards(commit_msgs, reveal_msgs)
 
     def _init_pid(self) -> None:
         n_player = len(self.consensus.node.player_list)
@@ -67,7 +69,7 @@ class DealingModule:
     def _generate_keys(self, n_cards: int) -> None:
         self._skey = gen_scalar_keypair()[0]
         self._tkeys = [
-            gen_scalar_keypair()[0] for _ in n_cards
+            gen_scalar_keypair()[0] for _ in range(n_cards)
         ]
         return
 
@@ -91,25 +93,36 @@ class DealingModule:
     def _listen_pass(self, msg_type: str) -> None:
         # TODO: from prev player or all players?
         while True:
-            raw_msg: RawMessage = self.consensus.node.consume_messages(
+            raw_msgs: List[RawMessage] = self.consensus.node.consume_messages(
                 msg_type=msg_type,
                 from_players=[self.prev_player_id],
                 expected_count=1
             )
+            if len(raw_msgs) != 1:
+                raise DealingError(
+                    f"_listen_pass: len(raw_msgs) = {len(raw_msgs)}, expected: 1"
+                )
             # TODO: use receive to verify?
-            if raw_msg.payload["to"] != self.pid:
+            if raw_msgs[0].payload["to"] != self.pid:
                 # TODO: should not happen?
                 continue
             break
-        self._points = raw_msg.payload["points"]
+        self._points = raw_msgs[0].payload["points"]
 
     def _listen_finaldeal(self) -> None:
-        raw_msg: RawMessage = self.consensus.node.consume_messages(
+        peers = self.consensus.node.peers()
+        raw_msgs: List[RawMessage] = self.consensus.node.consume_messages(
             msg_type=MSG_TYPE_FINALDEAL,
+            from_players=peers,
             expected_count=1
         )
+        if (len(raw_msgs) != len(peers)):
+            missing = set(peers) - {m.from_player for m in raw_msgs}
+            raise DealingError(
+                f"_listen_finaldeal: Missing finaldeal from: {missing}"
+            )
         # TODO: use receive to verify?
-        self._points = raw_msg.payload["points"]
+        self._points = raw_msgs[0].payload["points"]
 
     def _encrypt_shuffle(self, deck: List[int]) -> None:
         if self.pid == 0:
@@ -163,3 +176,60 @@ class DealingModule:
             point = decrypt_point(self._points[i], self._tkeys[i])
             self.hand.append(map_from_curve(point))
         return
+
+    def _commit_played_card(self, card: int) -> Tuple[bytes, List[RawMessage]]:
+        node = self.consensus.node
+        peers = node.peers()
+        # TODO: commit: hash(action | nonce | tkey)
+        nonce = self.consensus.commitment.commit(card)
+        commit_msgs: List[RawMessage] = node.consume_messages(
+            msg_type=MSG_TYPE_COMMIT,
+            from_players=peers,
+            expected_count=len(peers)
+        )
+        if len(commit_msgs) < len(peers):
+            missing = set(peers) - {m.from_player for m in commit_msgs}
+            raise PlayCardError(
+                f"play_card: Missing commits from: {missing}"
+            )
+        return (nonce, commit_msgs)
+
+    def _reveal_played_card(self, card: int, nonce: bytes) -> List[RawMessage]:
+        node = self.consensus.node
+        peers = node.peers()
+        # TODO: reveal: card, nonce, tkey
+        self.consensus.commitment.reveal(card, nonce)
+        reveal_msgs: List[RawMessage] = node.consume_messages(
+            msg_type=MSG_TYPE_REVEAL,
+            from_players=peers,
+            expected_count=len(peers)
+        )
+        if len(reveal_msgs) < len(peers):
+            missing = set(peers) - {m.from_player for m in reveal_msgs}
+            raise PlayCardError(
+                f"play_card: Missing reveals from: {missing}"
+            )
+        return reveal_msgs
+
+    def _verify_played_cards(self, commit_msgs: List[RawMessage],
+                                   reveal_msgs: List[RawMessage]):
+        # TODO: verify: card, nonce, tkey[i]
+        #   (1) hash(card | nonce | tkey[i]) == commit
+        #   (2) encrypt_point(map_to_curve(card), tkey[i]) == _points
+
+        # Map player_id -> hash from commit
+        peer_commits: Dict[str, str] = {
+            msg.from_player: msg.payload["hash"] for msg in commit_msgs
+        }
+        for msg in reveal_msgs:
+            player_id = msg.from_player
+            recv_action = msg.payload["action"]
+            recv_nonce = bytes.fromhex(msg.payload["nonce"])
+            expected_hash = peer_commits[player_id]
+            if not self.consensus.commitment.verify(recv_action,
+                                                    recv_nonce,
+                                                    expected_hash):
+                raise PlayCardError(
+                    f"_verify_played_cards: player '{player_id}' revealed "
+                    f"an action that does not match its commit"
+                )
