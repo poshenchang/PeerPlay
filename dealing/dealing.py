19943237f70cec7dab2fbf56a7f62c882c3679a5
shericWu <shihyu1127@gmail.com>
2026-05-27 16:59:48 +0800

    feat: add get_commit, reveal_card, get_cards in dealing module.

--- dealing/dealing.py
+++ dealing/dealing.py
@@ -30,41 +30,75 @@ class PlayCardError(Exception):
     Raised when play card cannot complete
     """
 
+
 class DealingModule:
+    """
+    Example usage: 
+        1. Initalize `dm = DealingModule(consensus)`
+        2. Every player calls `pid, hand = dm.deal()`
+        3. To play a card `dm.play_card(card)`
+        4. To reveal a card `dm.reveal_card(card)`
+        5. To get cards `dm.get_cards(enemy_pid, expect_count)`
+    """
     def __init__(self, consensus: ConsensusModule) -> None:
         self.consensus = consensus
-        self.pid: int = None                # The order for deal
+        self.pid: int = None                # The order pid in order
         self.prev_player_id: str = None     # Used for receive()
         self.hand: List[int] = []           # The hand dealt
+        self._order: List[int] = []         # The order for this deal
         self._points: List[Point] = []      # encrypted
         self._skey: int = None              # for shuffle, per deck
         self._tkeys: List[int] = []         # for tag, per card
+        # A list of played but not revealed (cards, nonce)
+        self._played_queue: Dict[int, bytes] = {}
+        # A nested dict for received but not verified commits
+        #     (player_id, (commit_id, message))
+        self._commit_queue: Dict[str, Dict[int, RawMessage]] = {}
 
-    def deal(self, deck: List[int], hand_size: int) -> List[int]:
+    def deal(self, deck: List[int], hand_size: int) -> Tuple[int, List[int]]:
         self._init_pid()
         self._generate_keys(len(deck))
         self._encrypt_shuffle(deck)
         self._tag()
         self._detag(hand_size)
         self._decrypt_hand(hand_size)
-        return self.hand
+        return (self.pid, self.hand)
 
     def play_card(self, card: int) -> None:
-        nonce, commit_msgs = self._commit_played_card(card)
-        reveal_msgs = self._reveal_played_card(card, nonce)
-        self._verify_played_cards(commit_msgs, reveal_msgs)
+        nonce = self._commit_played_card(card)
+        self._played_queue[card] = nonce
+        return
+
+    def get_commit(self, pid: int, expect_count: int) -> None:
+        commit_msgs = self._listen_commit(pid, expect_count)
+        return
+
+    def reveal_card(self, card: int) -> None:
+        nonce = self._played_queue.get(card)
+        self._reveal_played_card(card, nonce)
+        return
+
+    def get_cards(self, pid: int, expect_count: int) -> List[int]:
+        commit_msgs = self._listen_commit(pid, expect_count)
+        reveal_msgs = self._listen_reveal(pid, expect_count)
+        cards = self._verify_played_cards(commit_msgs, reveal_msgs)
+        return cards
 
     def _init_pid(self) -> None:
         n_player = len(self.consensus.node.player_list)
-        order = self.consensus.global_perm(list(range(n_player)))
-        player_idx = self.consensus.node.player_list.index(
-            self.consensus.node.player_id)
-        self.pid = order[player_idx]
-
+        self._order = self.consensus.global_perm(list(range(n_player)))
+        self.pid = self._player_id_to_pid(self.consensus.node.player_id)
         prev_pid = (self.pid - 1) if (self.pid > 0) else (n_player - 1)
-        prev_player_idx = order.index(prev_pid)
-        self.prev_player_id = self.consensus.node.player_list[prev_player_idx]
+        self.prev_player_id = self._pid_to_player_id(prev_pid)
         return
+
+    def _pid_to_player_id(self, pid: int) -> str:
+        idx = self._order.index(pid)
+        return self.consensus.node.player_list[idx]
+
+    def _player_id_to_pid(self, player_id: str) -> int:
+        idx = self.consensus.node.player_list.index(player_id)
+        return self._order[idx]
 
     def _generate_keys(self, n_cards: int) -> None:
         self._skey = gen_scalar_keypair()[0]
@@ -116,7 +150,7 @@ class DealingModule:
             from_players=peers,
             expected_count=1
         )
-        if (len(raw_msgs) != len(peers)):
+        if (len(raw_msgs) != 1):
             missing = set(peers) - {m.from_player for m in raw_msgs}
             raise DealingError(
                 f"_listen_finaldeal: Missing finaldeal from: {missing}"
@@ -157,7 +191,7 @@ class DealingModule:
             self._listen_pass(MSG_TYPE_DETAG)
 
         n_player = len(self.consensus.node.player_list)
-        for i in range(len(self._points) - n_player * hand_size):
+        for i in range(n_player * hand_size):
             if i in range(self.pid * hand_size, (self.pid + 1) * hand_size):
                 continue
             self._points[i] = decrypt_point(self._points[i], self._tkeys[i])
@@ -177,50 +211,58 @@ class DealingModule:
             self.hand.append(map_from_curve(point))
         return
 
-    def _commit_played_card(self, card: int) -> Tuple[bytes, List[RawMessage]]:
-        node = self.consensus.node
-        peers = node.peers()
+    def _commit_played_card(self, card: int) -> bytes:
         # TODO: commit: hash(action | nonce | tkey)
+        # TODO: add commit id
         nonce = self.consensus.commitment.commit(card)
-        commit_msgs: List[RawMessage] = node.consume_messages(
-            msg_type=MSG_TYPE_COMMIT,
-            from_players=peers,
-            expected_count=len(peers)
-        )
-        if len(commit_msgs) < len(peers):
-            missing = set(peers) - {m.from_player for m in commit_msgs}
-            raise PlayCardError(
-                f"play_card: Missing commits from: {missing}"
-            )
-        return (nonce, commit_msgs)
+        return nonce
 
-    def _reveal_played_card(self, card: int, nonce: bytes) -> List[RawMessage]:
-        node = self.consensus.node
-        peers = node.peers()
+    def _reveal_played_card(self, card: int, nonce: bytes) -> None:
         # TODO: reveal: card, nonce, tkey
         self.consensus.commitment.reveal(card, nonce)
+
+    def _listen_commit(self, pid: int, expect_count: int) -> List[RawMessage]:
+        node = self.consensus.node
+        from_player_id = self._pid_to_player_id(pid)
+        commit_msgs: List[RawMessage] = node.consume_messages(
+            msg_type=MSG_TYPE_COMMIT,
+            from_players=[from_player_id],
+            expected_count=expect_count
+        )
+        if len(commit_msgs) != expect_count:
+            raise PlayCardError(
+                f"play_card: Got {len(commit_msgs)} commits, "
+                f"expect: {expect_count}"
+            )
+        return commit_msgs
+
+    def _listen_reveal(self, pid: int, expect_count: int) -> List[RawMessage]:
+        node = self.consensus.node
+        from_player_id = self._pid_to_player_id(pid)
         reveal_msgs: List[RawMessage] = node.consume_messages(
             msg_type=MSG_TYPE_REVEAL,
-            from_players=peers,
-            expected_count=len(peers)
+            from_players=[from_player_id],
+            expected_count=expect_count
         )
-        if len(reveal_msgs) < len(peers):
-            missing = set(peers) - {m.from_player for m in reveal_msgs}
+        if len(reveal_msgs) != expect_count:
             raise PlayCardError(
-                f"play_card: Missing reveals from: {missing}"
+                f"play_card: Got {len(reveal_msgs)} commits, "
+                f"expect: {expect_count}"
             )
         return reveal_msgs
 
     def _verify_played_cards(self, commit_msgs: List[RawMessage],
-                                   reveal_msgs: List[RawMessage]):
+                             reveal_msgs: List[RawMessage]) -> List[int]:
         # TODO: verify: card, nonce, tkey[i]
         #   (1) hash(card | nonce | tkey[i]) == commit
         #   (2) encrypt_point(map_to_curve(card), tkey[i]) == _points
 
+        # TODO: currently index by player id, but multiple message sent by same player
         # Map player_id -> hash from commit
         peer_commits: Dict[str, str] = {
             msg.from_player: msg.payload["hash"] for msg in commit_msgs
         }
+        verified_actions: List[int] = []
         for msg in reveal_msgs:
             player_id = msg.from_player
             recv_action = msg.payload["action"]
@@ -233,3 +275,5 @@ class DealingModule:
                     f"_verify_played_cards: player '{player_id}' revealed "
                     f"an action that does not match its commit"
                 )
+            verified_actions.append(recv_action)
+        return verified_actions
